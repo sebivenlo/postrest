fd786a70cae1da66043872159f09c6920c63d91c
Pieter van den Hombergh (homberghp) <p.vandenhombergh@fontys.nl>
2019-04-14 14:20:35 +0200

    add localdate adapter

---
+++ studentgenerator/src/main/java/nl/fontys/sebivenlo/randomstudents/LocalDateJsonAdapter.java
@@ -0,0 +1,35 @@
+package nl.fontys.sebivenlo.randomstudents;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Pieter van den Hombergh {@code pieter.van.den.hombergh@gmail.com}
+ */
+public class LocalDateJsonAdapter implements JsonSerializer<LocalDate>,
+        JsonDeserializer<LocalDate> {
+
+    public LocalDateJsonAdapter() {
+    }
+
+    @Override
+    public JsonElement serialize( LocalDate src, Type typeOfSrc,
+            JsonSerializationContext context ) {
+        return new JsonPrimitive( src.toString() );
+    }
+
+    @Override
+    public LocalDate deserialize( JsonElement json, Type typeOfT,
+            JsonDeserializationContext context ) throws JsonParseException {
+        return LocalDate.parse( json.getAsString() );
+    }
+
+}
